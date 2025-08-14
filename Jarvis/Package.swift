8d89889261df3e5431aa3fe273e537307dacf9b7
mistico06 <adityanampally1306@gmail.com>
2025-08-15 01:51:07 +0530

    tweaks

--- Jarvis/Package.swift
+++ Jarvis/Package.swift
@@ -1,30 +1,35 @@
-// swift-tools-version: 6.0
-import PackageDescription
+name: Build iOS App
 
-let package = Package(
-    name: "Jarvis",
-    platforms: [
-        .iOS(.v16),
-    ],
-    products: [
-        .executable(name: "Jarvis", targets: ["Jarvis"])
-    ],
-    dependencies: [
-        // Local dependency: mlc-llm cloned in ThirdParty
-        .package(path: "ThirdParty/mlc-llm/ios/MLCSwift"),
-        // Other dependencies
-        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0"..<"4.0.0"),
-        .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.14.1")
-    ],
-    targets: [
-        .executableTarget(
-            name: "Jarvis",
-            dependencies: [
-                .product(name: "MLCSwift", package: "MLCSwift"),
-                .product(name: "Crypto", package: "swift-crypto"),
-                .product(name: "SQLite", package: "SQLite.swift")
-            ],
-            path: "Sources/App"
-        )
-    ]
-)
+on:
+  push:
+    branches: [ "main" ]
+  workflow_dispatch:
+
+jobs:
+  build:
+    runs-on: macos-latest
+
+    steps:
+      - name: Checkout repo with submodules
+        uses: actions/checkout@v3
+        with:
+          submodules: recursive
+
+      - name: Set up Xcode 16
+        uses: maxim-lobanov/setup-xcode@v1
+        with:
+          xcode-version: '16.2'
+
+      - name: List available schemes
+        working-directory: Jarvis
+        run: xcodebuild -list
+
+      - name: Build iOS package
+        working-directory: Jarvis
+        run: |
+          xcodebuild clean build \
+            -scheme Jarvis \
+            -destination "generic/platform=iOS" \
+            IPHONEOS_DEPLOYMENT_TARGET=16.0 \
+            CODE_SIGN_STYLE=Automatic \
+            -allowProvisioningUpdates
