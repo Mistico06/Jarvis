3bb4035b67064032fa41ccf054ca872be70344e9
Mistico <44604778+Mistico06@users.noreply.github.com>
2025-08-14 08:05:17 +0530

    Delete Jarvis/Tests directory

--- Jarvis/Tests/JarvisTests.swift
+++
@@ -1,8 +0,0 @@
-import XCTest
-@testable import App  // 'App' is your main target name from Package.swift
-
-final class JarvisTests: XCTestCase {
-    func testExample() {
-        XCTAssertTrue(true)
-    }
-}
