d45d0fad01bea36976a8ace9c96358c5ffeadd48
Mistico <44604778+Mistico06@users.noreply.github.com>
2025-08-14 07:37:12 +0530

    Create JarvisTests.swift

---
+++ Jarvis/Tests/JarvisTests.swift
@@ -0,0 +1,8 @@
+import XCTest
+@testable import App  // 'App' is your main target name from Package.swift
+
+final class JarvisTests: XCTestCase {
+    func testExample() {
+        XCTAssertTrue(true)
+    }
+}
