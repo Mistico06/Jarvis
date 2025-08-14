09881e790e58cbe3c7eb323379afdd4b4692a14c
Mistico <44604778+Mistico06@users.noreply.github.com>
2025-08-14 07:14:58 +0530

    Update Package.swift

--- Jarvis/Package.swift
+++ Jarvis/Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "Jarvis",
     platforms: [
-        .iOS(.v18)
+        .iOS(18.0)
     ],
     products: [
         .executable(name: "Jarvis", targets: ["App"])
